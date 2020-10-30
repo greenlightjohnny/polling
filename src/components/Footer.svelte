28409ade93eccb26308052bec096d551c05988ca
fj <castelloiv@gmail.com>
2020-10-29 22:50:57 -0400

    Working on styling

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+
+  .copyright {
+    color: #aaa;
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px;
+  }
+</style>
+
+<footer>
+  <div class="copyright">CopyRight 2020 Beacon Bird</div>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 0px;
+  }
+
+  .imageCon {
+    margin: 0 auto;
+    max-width: 90px;
+  }
+  img {
+    height: 90px;
+    margin: 0 auto;
+  }
+</style>
+
+<header>
+  <div class="imageCon"><img src="/img/birdy.png" alt="Poll image" /></div>
+</header>
